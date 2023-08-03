f7a86cbdaa9d86df04f80469aaab5232bda326cb
newbriskCulinary <141027087+newbriskCulinary@users.noreply.github.com>
2023-08-03 10:54:33 +0100

    Add files via upload
    i made some major changes to the website

--- script.js
+++ script.js
@@ -20,3 +20,16 @@ document.querySelectorAll('.navbar-nav .nav-link, .navbar-toggler').forEach((ite
         }
     });
 });
+
+
+var i = 0;
+var txt = 'Our team of seasoned chefs and industry experts are dedicated to guiding you through a comprehensive curriculum that covers everything from classic culinary techniques to cutting-edge gastronomy.Whether you dream of becoming a master chef, opening your own restaurant, or simply want to elevate your cooking skills to impress your friends and family, New and Brisk is here to make that dream a reality. Our hands-on approach, state-of-the-art facilities, and a diverse range of courses will empower you to unleash your culinary potential Join us on this gastronomic adventure, where you"ll not only learn the art of cooking but also discover the science behind it. Let"s embark on a culinary journey together and create delightful memories that linger on the taste buds of those you serve Come and be a part of the New and Brisk family, where passion meets perfection, and culinary dreams come true.';
+var speed = 60;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
